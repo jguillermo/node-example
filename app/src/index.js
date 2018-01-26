6541cbb13133c2d66a46f2a4820817390532915f
Jose Guillermo <jguillermo@outlook.com>
2018-01-26 15:17:11 -0500

    log

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import morgan from 'morgan'
+import bodyParse from 'body-parser'
 
 const app = express()
+app.disable('X-Powered-By')
+
+app.use(morgan('combined'))
+app.use(bodyParse.json())
+app.use(bodyParse.urlencoded({extended:false}))
 
 app.get('/',(req,res)=>{
     res.end('Hola mundo')
